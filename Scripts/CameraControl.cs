88aff89d449a9a0d35185036800ebe060e231dc3
Zaoxi <whgmlwo1000@gmail.com>
2018-03-13 21:50:41 +0900

    3인칭카메라, 플레이어 캐릭터 구현
    3인칭 카메라 움직임 밑 회전 구현
    - 마우스 회전에 따라 카메라 회전 및 이동
    플레이어 캐릭터가 움직일 때, 카메라의 방향으로 회전하면서 움직이도록 구현

---
+++ Scripts/CameraControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float sensitibity;
+    
+    
+    private GameObject playerObject;
+    private Vector3 playerPosition;
+    private float mouseAngleX = 0.0f;
+    private float mouseAngleY = 0.0f;
+
+    
+
+    // 마우스 수직 이동 허용 범위
+    private const float MOUSE_VERTICAL_HEIGHT_MAX = 0.8f;
+    private const float MOUSE_VERTICAL_HEIGHT_MIN = -0.6f;
+    // 마우스 수직 이동에 대한 회전값
+    private const float MOUSE_VERTICAL_ROTATION_SPEED = 70f;
+    private const float MOUSE_VERTICAL_BASE_SENSITIVITY = 0.015f;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Mouse X");
+        float moveY = Input.GetAxis("Mouse Y");
+
+        CheckMouseMove(moveX, moveY);
+        
+    }
+
+    private void CheckMouseMove(float moveX, float moveY)
+    {
+        mouseAngleX += -moveX * sensitibity;
+        mouseAngleX %= 360;
+        float tempAngle = mouseAngleY + -moveY * sensitibity * MOUSE_VERTICAL_BASE_SENSITIVITY;
+        if (tempAngle < MOUSE_VERTICAL_HEIGHT_MAX && tempAngle > MOUSE_VERTICAL_HEIGHT_MIN)
+        {
+            mouseAngleY = tempAngle;
+            mouseAngleY %= 360;
+        }
+
+        float mouseMovementX = mouseAngleX * Mathf.PI / 180.0f;
+        float mouseMovementY = mouseAngleY * Mathf.PI / 180.0f;
+
+        // 마우스 좌우 이동과 상하 이동에 삼각함수를 적용, 자세한 설명은 카메라 명세서 참조
+        transform.position = new Vector3(Mathf.Cos(mouseMovementX) + Mathf.Sqrt(3) * Mathf.Sin(mouseMovementX) + playerObject.transform.position.x,
+            playerObject.transform.position.y + PlayerConstant.PLAYER_HEIGHT + Mathf.Sqrt(3) * Mathf.Tan(mouseAngleY), Mathf.Sin(mouseMovementX) - Mathf.Sqrt(3) * Mathf.Cos(mouseMovementX) + playerObject.transform.position.z);
+
+        transform.rotation = Quaternion.Euler(mouseAngleY * MOUSE_VERTICAL_ROTATION_SPEED, -mouseAngleX, 0.0f);
+    }
+}
---
+++ Scripts/PlayerControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    private GameObject playerCamera;
+    private Rigidbody rb;
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveV = Input.GetAxis("Vertical") * speed;
+        float moveH = Input.GetAxis("Horizontal") * speed;
+        moveV *= Time.deltaTime;
+        moveH *= Time.deltaTime;
+
+        if (moveV != 0 || moveH != 0)
+        {
+            //transform.Translate(moveH, 0.0f, moveV);
+            //rb.MoveRotation(Quaternion.Euler(0.0f, playerCamera.transform.rotation.eulerAngles.y, 0.0f));
+            //transform.rotation = Quaternion.Euler(0.0f, playerCamera.transform.eulerAngles.y, 0.0f);
+            if(playerCamera.transform.eulerAngles.y != transform.eulerAngles.y)
+            {
+                float angleTerm = playerCamera.transform.eulerAngles.y - transform.eulerAngles.y;
+                if(angleTerm > 180f)
+                {
+                    angleTerm = playerCamera.transform.eulerAngles.y - (transform.eulerAngles.y + 360f);
+                }
+                else if(angleTerm < -180f)
+                {
+                    angleTerm = playerCamera.transform.eulerAngles.y + 360f - transform.eulerAngles.y;
+                }
+                else
+                {
+                    angleTerm = playerCamera.transform.eulerAngles.y - transform.eulerAngles.y;
+                }
+
+                transform.Rotate(0f, angleTerm / 10, 0f);
+            }
+            transform.Translate(moveH, 0.0f, moveV);
+        }
+        
+        
+
+
+    }
+}
+
+
+public static class PlayerConstant
+{
+    public static float PLAYER_HEIGHT = 1.2f;
+}
